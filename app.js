9618b3e010c5d093280462eb0d81f0ab45a8cafd
Darshan Dhargalkar <darshandharaglkar@gmail.com>
2026-08-29 19:11:22 +0530

    Implement game winner display and enhance UI elements

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ const winPatterns = [
 
 boxes.forEach((box) => {
     box.addEventListener("click", () => {
+        console.log('clicked');
         if (turno) {
             box.innerText = "O"
             turno = false
@@ -33,6 +34,13 @@ boxes.forEach((box) => {
     })
 })
 
+const showWinner = (winner) => {
+    msg.innerHTML = `Congratulations , Winner is ${winner}`
+    msgContainer.classList.remove("hide")
+
+}
+
+
 const checkWinner = () => {
     for (let pattern of winPatterns) {
         let pos1Val = boxes[pattern[0]].innerText;
@@ -42,121 +50,12 @@ const checkWinner = () => {
         if (pos1Val != "" && pos2Val != "" && pos3Val != "") {
             if (pos1Val === pos2Val && pos2Val === pos3Val) {
                 console.log('win');
-
                 showWinner(pos1Val);
-
 
             }
         }
     }
-}
-
-
-// const showWinner = (winner) => {
-//     msg.innerHTML = `Congratulations , Winner is ${winner}`
-//     msgContainer.classList.remove("hide")
-//     disableBoxes();
-//     celebrate();
-// }
-
-
-const showWinner = (winner) => {
-
-    msg.innerHTML = `🎉 Congratulations! 🎉 <br> Winner is ${winner} 🏆`;
-
-    msgContainer.classList.remove("hide");
-
-    disableBoxes();
-
-};
-
-const disableBoxes = () => {
-    for (let box of boxes) {
-        box.disabled = true;
-    }
-}
-
-newGameBtn.addEventListener("click", resetGame)
-resetbtn.addEventListener("click", resetGame)
-
-const resetGame = () => {
-    turno = true;
-    enableBoxes();
-    msgContainer.classList.add("hide");
 
 }
 
-const enableBoxes = () => {
-    for (let box of boxes) {
-        box.disabled = false;
-        box.innerText = "";
-    }
-}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const celebrate = () => {
-
-    // Big celebration container
-    const celebration = document.createElement("div");
-    celebration.classList.add("celebration");
-
-    celebration.innerHTML = `
-        <div class="celebration-text">
-            🎉🎊🏆🎊🎉
-        </div>
-    `;
-
-    document.body.appendChild(celebration);
-
-    // Create confetti
-    const colors = [
-        "#ff0000",
-        "#00ff00",
-        "#0000ff",
-        "#ffff00",
-        "#ff00ff",
-        "#00ffff",
-        "#ff8800"
-    ];
-
-    for (let i = 0; i < 150; i++) {
-
-        const confetti = document.createElement("div");
-
-        confetti.classList.add("confetti");
-
-        confetti.style.left = Math.random() * 100 + "vw";
-        confetti.style.backgroundColor =
-            colors[Math.floor(Math.random() * colors.length)];
-
-        confetti.style.animationDuration =
-            Math.random() * 3 + 2 + "s";
-
-        confetti.style.animationDelay =
-            Math.random() * 1.5 + "s";
-
-        document.body.appendChild(confetti);
-
-        setTimeout(() => {
-            confetti.remove();
-        }, 6000);
-    }
-
-    // Remove celebration after 5 seconds
-    setTimeout(() => {
-        celebration.remove();
-    }, 5000);
-};
